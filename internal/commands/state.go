31eb7493eba0d08520faa7175c899ad925703f3f
NeroQue <dominikbratek11@gmail.com>
2025-07-21 19:39:50 +0200

    Add command handling with login command implementation

---
+++ internal/commands/state.go
@@ -0,0 +1,7 @@
+package commands
+
+import "github.com/NeroQue/GATOR/internal/config"
+
+type State struct {
+	CFG *config.Config
+}
--- main.go
+++ main.go
@@ -2,25 +2,42 @@ package main
 
 import (
 	"fmt"
+	"github.com/NeroQue/GATOR/internal/commands"
 	"github.com/NeroQue/GATOR/internal/config"
+	"os"
+
 	"log"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "error: not enough arguments\n")
+		os.Exit(1)
+	}
+
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
-	fmt.Printf("Read config: %+v\n", cfg)
-
-	err = cfg.SetUser("Nero")
-	if err != nil {
-		log.Fatalf("couldn't set current user: %v", err)
+	state := &commands.State{
+		CFG: &cfg,
 	}
 
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalf("error reading config: %v", err)
+	cmds := commands.Commands{
+		Commands: make(map[string]func(*commands.State, commands.Command) error),
 	}
-	fmt.Printf("Read config again: %+v\n", cfg)
+	err = cmds.Register("login", commands.HandlerLogin)
+	if err != nil {
+		log.Fatalf("error registering command: %v", err)
+	}
+
+	cmd := commands.Command{
+		Name: os.Args[1],
+		Args: os.Args[2:],
+	}
+
+	if err := cmds.Run(state, cmd); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
 }
